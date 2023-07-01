2aa7e582ca982848b0959353f464bd5ffa3ee7da
Artur Sheshukov <arti.shishka11@gmail.com>
2023-07-01 12:47:48 +0600

    Обновил админку вылетала ошибка по полям дат

--- guest_book/admin.py
+++ guest_book/admin.py
@@ -8,6 +8,7 @@ class GuestBookAdmin(admin.ModelAdmin):
     list_filter = ['guest_name', 'status', 'create_date', 'update_date']
     list_display_links = ['id', 'guest_name', 'guest_email', 'status']
     search_fields = ['guest_name', 'guest_email']
+    readonly_fields = ['create_date', 'update_date']
     fields = ['guest_name', 'guest_email', 'text_records', 'create_date', 'update_date', 'status']
 
 
