140e61c5e80b7174e37c86be843f595ee20141ec
Artur Sheshukov <arti.shishka11@gmail.com>
2023-07-01 12:57:05 +0600

    Обновил модель GuestBook с даты на дату и время, создавл фиксутры

--- guest_book/models.py
+++ guest_book/models.py
@@ -7,14 +7,13 @@ class GuestBook(models.Model):
     guest_name = models.CharField(max_length=50, null=False, blank=False, verbose_name='Имя гостя')
     guest_email = models.EmailField(null=False, blank=False, verbose_name='Email гостя')
     text_records = models.TextField(max_length=300, null=True, blank=True, verbose_name='Доп. описание')
-    create_date = models.DateField(auto_now_add=True, verbose_name='Дата создания')
-    update_date = models.DateField(auto_now=True, verbose_name='Дата обновления')
+    create_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    update_date = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
     status = models.CharField(null=False, blank=False, default='active',
                               verbose_name='Статус', choices=status_field)
 
     def __str__(self):
-        return f'{self.guest_name} - {self.guest_email} - {self.text_records} - {self.create_date}' \
-               f' - {self.update_date} - {self.status}'
+        return f'{self.guest_name} - {self.guest_email}'
 
     class Meta:
         db_table = 'guestbook'
